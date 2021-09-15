9e76b949fc2fca6fddac7af4ed184c134e104f2d
Alexis Guzman <alexisguzman157@outlook.com>
2021-09-14 23:52:36 -0400

    added gen html

--- Starter_10-OOP HW/index.js
+++ Starter_10-OOP HW/index.js
@@ -5,42 +5,101 @@ const fs = require('fs');
 
 const generateMarkdown = require('./utils/generateMarkdown')
 
-// const employee = require('');
+ const employee = require('./Employee');
 // const manager = require('');
 // const engineer = require('');
 // const intern = require('');
 
+employeePrompts();
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Enter name of manager',
-            name: 'name'
-        },
-        {
-            type: 'input',
-            message: 'input ID',
-            name: 'id'
-        },
-        {
-            type: 'input',
-            message: 'input email',
-            name: 'email'
-        },
-        {
-            type: 'input',
-            message:'enter office number',
-            name: 'office'
-        },
-    ]).then(answers => {
-        let something = generateMarkdown(answers)
-        fs.writeFile('test.html', something,
-            (err) => {
-                if (err) throw err;
-            })
-    }
-    );
+function employeePrompts() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter name of manager',
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: 'input title',
+                name: 'title'
+            },
+            {
+                type: 'input',
+                message: 'input ID',
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: 'input email',
+                name: 'email'
+            },
+            {
+                type: 'input',
+                message: 'enter office number',
+                name: 'office'
+            },
+            {
+                type: 'input',
+                message: 'Enter name of Intern',
+                name: 'name1'
+            },
+            {
+                type: 'input',
+                message: 'enter title',
+                name: 'title1'
+            },
+            {
+                type: 'input',
+                message: 'input ID',
+                name: 'id1'
+            },
+            {
+                type: 'input',
+                message: 'input email',
+                name: 'email1'
+            },
+            {
+                type: 'input',
+                message: 'School',
+                name: 'school'
+            },
+            {
+                type: 'input',
+                message: 'Enter name of engineer',
+                name: 'name2'
+            },
+            {
+                type: 'input',
+                message: 'enter title',
+                name: 'title2'
+            },
+            {
+                type: 'input',
+                message: 'input ID',
+                name: 'id2'
+            },
+            {
+                type: 'input',
+                message: 'input email',
+                name: 'email2'
+            },
+            {
+                type: 'input',
+                message: 'enter github',
+                name: 'github'
+            },
+        ]).then(answers => {
+            let something = generateMarkdown(answers)
+            fs.writeFile('test.html', something,
+                (err) => {
+                    if (err) throw err;
+                })
+        }
+        );
+}
+
 
     // const genHtml =(html) => {
     //     fs.writeFile('test.html', html, (err) => {
@@ -49,7 +108,7 @@ inquirer
     //     })
     // }
 
-    
+
 
 //     inquirer
 //     .prompt([
